9589a4a64a25dc0743972bbef38ec6c19418ae88
punyaswaroop12 <punyaswaroop12@users.noreply.github.com>
2022-07-08 23:22:59 -0400

    added trees and heaps

---
+++ coding_practice_questions/Heap_coding.py
@@ -0,0 +1,33 @@
+# Merge k Sorted Lists
+#####################################################################################################################################
+# https://leetcode.com/problems/merge-k-sorted-lists/
+
+
+# Sliding Window Maximum
+#####################################################################################################################################
+# https://leetcode.com/problems/sliding-window-maximum/
+
+
+# Find Median from Data Stream
+#####################################################################################################################################
+# https://leetcode.com/problems/find-median-from-data-stream/
+
+
+# Top K Frequent Elements
+#####################################################################################################################################
+# https://leetcode.com/problems/top-k-frequent-elements/
+
+
+# Sort Characters By Frequency
+#####################################################################################################################################
+# https://leetcode.com/problems/sort-characters-by-frequency/
+
+
+# Kth Largest Element in a Stream
+#####################################################################################################################################
+# https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+
+# Process Tasks Using Servers
+#####################################################################################################################################
+#https://leetcode.com/problems/process-tasks-using-servers/
---
+++ coding_practice_questions/trees_coding.py
@@ -0,0 +1,91 @@
+# Lowest Common Ancestor of a Binary Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree
+
+# Binary Tree Maximum Path Sum
+#####################################################################################################################################
+# https://leetcode.com/problems/binary-tree-maximum-path-sum
+
+# Binary Tree Right Side View
+#####################################################################################################################################
+# https://leetcode.com/problems/binary-tree-right-side-view
+
+# Serialize and Deserialize Binary Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/serialize-and-deserialize-binary-tree
+
+# All Nodes Distance K in Binary Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree
+
+# Count Good Nodes in Binary Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/count-good-nodes-in-binary-tree
+
+
+# Lowest Common Ancestor of a Binary Tree III
+#####################################################################################################################################
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii
+
+# Validate Binary Search Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/validate-binary-search-tree
+
+
+# Vertical Order Traversal of a Binary Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree
+
+# Convert Binary Search Tree to Sorted Doubly Linked List
+#####################################################################################################################################
+# https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list
+
+# Diameter of Binary Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/diameter-of-binary-tree
+
+
+# Binary Tree Zigzag Level Order Traversal
+#####################################################################################################################################
+# https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal
+
+# Invert Binary Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/invert-binary-tree
+
+# Balance a Binary Search Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/balance-a-binary-search-tree
+
+# Binary Tree Level Order Traversal
+#####################################################################################################################################
+# https://leetcode.com/problems/binary-tree-level-order-traversal
+
+# Flatten Binary Tree to Linked List
+#####################################################################################################################################
+# https://leetcode.com/problems/flatten-binary-tree-to-linked-list
+
+# Construct Binary Tree from Preorder and Inorder Traversal
+#####################################################################################################################################
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+
+# Symmetric Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/symmetric-tree
+
+# Unique Binary Search Trees
+#####################################################################################################################################
+# https://leetcode.com/problems/unique-binary-search-trees
+
+# Cousins in Binary Tree
+#####################################################################################################################################
+# https://leetcode.com/problems/cousins-in-binary-tree
+
+
+# Construct Binary Search Tree from Preorder Traversal
+#####################################################################################################################################
+# https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal
+
+# Inorder Successor in BST
+#####################################################################################################################################
+# https://leetcode.com/problems/inorder-successor-in-bst
